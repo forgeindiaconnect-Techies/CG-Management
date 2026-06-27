e4ac307455ca2aecf3c5f86977a69c1162addb89
Forge IndiaConnect <forgeindiaconnectfic@gmail.com>
2026-06-27 12:19:45 +0530

    Fix middleware import in supportRoutes

--- backend/routes/supportRoutes.js
+++ backend/routes/supportRoutes.js
@@ -5,13 +5,13 @@ const {
   getMessages,
   updateMessageStatus,
 } = require('../controllers/supportController');
-const { protect, admin } = require('../middleware/authMiddleware');
+const { protect, authorize } = require('../middleware/authMiddleware');
 
 // Public or User route (we can use protect but let's allow it to be optional by just using protect for now, since it's in the user dashboard)
 router.post('/', protect, createMessage);
 
 // Admin routes
-router.get('/', protect, admin, getMessages);
-router.put('/:id/status', protect, admin, updateMessageStatus);
+router.get('/', protect, authorize('Admin'), getMessages);
+router.put('/:id/status', protect, authorize('Admin'), updateMessageStatus);
 
 module.exports = router;
